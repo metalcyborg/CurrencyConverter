26af87276f54267fd155d6351bec8580d4f55985
Peter Trushkov <petertrushkov@gmail.com>
2017-08-02 11:18:50 +0500

    Add project structure

---
+++ app/src/main/java/com/metalcyborg/currencyconverter/converter/ConverterActivity.java
@@ -0,0 +1,15 @@
+package com.metalcyborg.currencyconverter.converter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.metalcyborg.currencyconverter.R;
+
+public class ConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_converter);
+    }
+}
---
+++ app/src/main/java/com/metalcyborg/currencyconverter/model/Currency.java
@@ -0,0 +1,8 @@
+package com.metalcyborg.currencyconverter.model;
+
+/**
+ * Created by metalcyborg on 02.08.17.
+ */
+
+public class Currency {
+}
