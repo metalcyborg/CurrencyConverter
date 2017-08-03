6fd073bb201515b2bc91ef89e8ad803872e2013f
Peter Trushkov <petertrushkov@gmail.com>
2017-08-03 13:14:16 +0500

    CurrencyListLoader call

--- app/src/main/java/com/metalcyborg/currencyconverter/converter/ConverterPresenter.java
+++ app/src/main/java/com/metalcyborg/currencyconverter/converter/ConverterPresenter.java
@@ -1,20 +1,35 @@
 package com.metalcyborg.currencyconverter.converter;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 
 import com.metalcyborg.currencyconverter.model.Currency;
+import com.metalcyborg.currencyconverter.model.source.CurrencyListLoader;
 import com.metalcyborg.currencyconverter.model.source.CurrencyModel;
 import com.metalcyborg.currencyconverter.util.ConverterUtil;
 
-public class ConverterPresenter implements ConverterContract.Presenter {
+import java.util.List;
 
-    private CurrencyModel mModel;
-    private ConverterContract.View mView;
+public class ConverterPresenter implements ConverterContract.Presenter,
+        LoaderManager.LoaderCallbacks<List<Currency>> {
+
+    private static final int CURRENCY_QUERY = 1;
+
+    private final CurrencyListLoader mLoader;
+    private final LoaderManager mLoaderManager;
+    private final CurrencyModel mModel;
+    private final ConverterContract.View mView;
     private Currency mCurrencyFrom;
     private Currency mCurrencyTo;
 
-    public ConverterPresenter(@NonNull CurrencyModel model,
+    public ConverterPresenter(@NonNull CurrencyListLoader loader,
+                              @NonNull LoaderManager loaderManager,
+                              @NonNull CurrencyModel model,
                               @NonNull ConverterContract.View view) {
+        mLoader = loader;
+        mLoaderManager = loaderManager;
         mModel = model;
         mView = view;
         mView.setPresenter(this);
@@ -23,7 +38,7 @@ public class ConverterPresenter implements ConverterContract.Presenter {
     @Override
     public void start() {
         // Load currency data
-
+        mLoaderManager.initLoader(CURRENCY_QUERY, null, this);
     }
 
     @Override
@@ -45,5 +60,22 @@ public class ConverterPresenter implements ConverterContract.Presenter {
     @Override
     public void setCurrencyTo(Currency currency) {
         mCurrencyTo = currency;
+    }
+
+    @Override
+    public Loader<List<Currency>> onCreateLoader(int id, Bundle args) {
+        mView.setProgressVisibility(true);
+        return mLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Currency>> loader, List<Currency> data) {
+        mView.addCurrencies(data);
+        mView.setProgressVisibility(false);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Currency>> loader) {
+
     }
 }
